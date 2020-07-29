96dc8877418ab46a28d121c959d5849a6ed45610
muthuism <muthuism.09@gmail.com>
2020-07-29 21:18:30 +0530

    PPTM Commit

---
+++ PPMToolFullStack/src/main/java/com/PPMToolFullStack/PPMToolFullStack/PpmToolFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.PPMToolFullStack.PPMToolFullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullStackApplication.class, args);
+	}
+
+}
