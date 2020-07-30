a7b1db361427d5e8f7a11f9cc919bb857cc937d9
muthuism <muthuism.09@gmail.com>
2020-07-30 07:50:18 +0530

    Add project commit

---
+++ PPMToolFullStack/src/main/java/com/PPMTool/repository/ProjectRepository.java
@@ -0,0 +1,15 @@
+package com.PPMTool.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.PPMTool.entity.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	 Project findByProjectIdentifier(String projectId);
+
+	    @Override
+	    Iterable<Project> findAll();
+}
